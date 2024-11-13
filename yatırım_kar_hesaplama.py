91b3aa22277998a2321a004194ccb7a3377bea59
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-11-13 21:03:06 +0300

    Bugfix, yatırım kar hesaplama eklendi.

--- yatırım_kar_hesaplama.py
+++ yatırım_kar_hesaplama.py
@@ -7,18 +7,13 @@ from selenium.webdriver.support import expected_conditions as ec
 
 DK_INVESTMENTS = {
     "dollar": 280,
-    "euro": 1050,
     "quarter gold": 2,
     "1gr gold (22K)": 3,
     "1gr gold (24K)": 7,
     "0,5gr gold (24K)": 2,
 }
 
-VK_INVESTMENTS = {
-    "dollar": 200,
-    "euro": 400,
-    "quarter gold": 3
-}
+DK_TOPLAM_YATIRIM = 41279
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -39,10 +34,6 @@ try:
 except Exception as e:
     print(f"Reklam kapatılamadı: {e}")
 
-def kar_hesapla():
-    price_gr_gold_22K = driver.find_element(By.XPATH, value='//*[@id="altinfiyat"]/tbody/tr[15]/td[3]').text
-
-
 
 price_gr_gold_22K = driver.find_element(By.XPATH, value='//*[@id="altinfiyat"]/tbody/tr[15]/td[3]').text
 price_gr_gold_22K = float(price_gr_gold_22K.strip(" TL").replace(".", ""))
@@ -52,41 +43,32 @@ price_gr_gold_24K = driver.find_element(By.XPATH, value='//*[@id="altinfiyat"]/t
 price_gr_gold_24K = float(price_gr_gold_24K.strip(" TL").replace(".", ""))
 print(price_gr_gold_24K)
 
-price_05_gr_gold_24K = driver.find_element(By.XPATH, value='/html/body/div[13]/div[6]/div[2]/div[3]/table/tbody/tr[4]/td[3]').text
+price_05_gr_gold_24K = driver.find_element(By.XPATH,
+                                           value='/html/body/div[13]/div[6]/div[2]/div[3]/table/tbody/tr[4]/td[3]').text
 price_05_gr_gold_24K = float(price_05_gr_gold_24K.strip(" TL").replace(".", ""))
 print(price_05_gr_gold_24K)
 
 price_quarter_gold = driver.find_element(By.XPATH, value='//*[@id="altinfiyat"]/tbody/tr[3]/td[3]').text
-price_quarter_gold = float(price_quarter_gold .strip(" TL").replace(".", ""))
+price_quarter_gold = float(price_quarter_gold.strip(" TL").replace(".", ""))
 print(price_quarter_gold)
 
 price_dollar = driver.find_element(By.XPATH, value='//*[@id="usd_header_son_data"]').text
 price_dollar = float(price_dollar.replace(",", "."))
 print(price_dollar)
 
-price_euro = driver.find_element(By.XPATH, value='//*[@id="eur_header_son_data"]').text
-price_euro = float(price_euro.replace(",", "."))
-
 dk_total = 0
 dk_total += DK_INVESTMENTS["dollar"] * price_dollar
-dk_total += DK_INVESTMENTS["euro"] * price_euro
 dk_total += DK_INVESTMENTS["quarter gold"] * price_quarter_gold
 dk_total += DK_INVESTMENTS["1gr gold (22K)"] * price_gr_gold_22K
 dk_total += DK_INVESTMENTS["1gr gold (24K)"] * price_gr_gold_24K
 dk_total += DK_INVESTMENTS["0,5gr gold (24K)"] * price_05_gr_gold_24K
 
-
-vk_total = 0
-vk_total += VK_INVESTMENTS["dollar"] * price_dollar
-vk_total += VK_INVESTMENTS["euro"] * price_euro
-vk_total += VK_INVESTMENTS["quarter gold"] * price_quarter_gold
-
 current_time = dt.now()
 formatted_time = current_time.strftime("%d %B %Y, %H:%M:%S")
 
-with open("values.txt", "a") as file:
-    file.write(f"\nDodo:{dk_total}\nVural:{vk_total}\n{formatted_time}\nTotal:{dk_total + vk_total}\n")
+with open("degerler.txt", "a") as file:
+    file.write(f"\nDodo:{dk_total}\n{formatted_time}\nTotal:{dk_total}\nOrjinal Yatırım Değeri:{DK_TOPLAM_YATIRIM}\nKAR:{dk_total - DK_TOPLAM_YATIRIM}")
 
-print(f"dk_total={dk_total}\nvk_total ={vk_total}")
+print(f"dk_total={dk_total}\n")
 
 driver.close()
